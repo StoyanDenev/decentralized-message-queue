633fc4c97d982114f0ad78d2882bd171fb27cfa1
Stoyan Denev <StoyanDenev@github.com>
2026-09-23 00:26:40 +0300

    fix(win32): add bcrypt to EXTRA_LIBS and protect ssize_t and sockets in virtual_transport.h

--- include/determ/net/virtual_transport.h
+++ include/determ/net/virtual_transport.h
@@ -13,7 +13,12 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+#if defined(_WIN32)
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#else
 #include <sys/types.h>
+#endif
 #include "determ/net/event_loop.h"
 
 #ifdef __cplusplus
@@ -51,7 +56,11 @@ ssize_t determ_net_send(int fd, const void *buf, size_t len, int flags);
 #else
 
 /* Production zero-cost pass-throughs */
+#if defined(_WIN32)
+#include <winsock2.h>
+#else
 #include <sys/socket.h>
+#endif
 
 #define determ_net_recv(fd, buf, len, flags) recv((fd), (buf), (len), (flags))
 #define determ_net_send(fd, buf, len, flags) send((fd), (buf), (len), (flags))
